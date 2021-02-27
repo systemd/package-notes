68fd73c169b1ce8928ef2069f49e696a275c1479
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-02-27 12:25:57 +0100

    generate-package-notes v1

---
+++ generate-package-notes.py
@@ -0,0 +1,102 @@
+"""
+SECTIONS
+{
+    .note.package.name ALIGN(8): {
+        BYTE(0x08) BYTE(0x00) BYTE(0x00) BYTE(0x00) /* Length of line 4 including NUL */
+        BYTE(0x08) BYTE(0x00) BYTE(0x00) BYTE(0x00) /* Length of line 5 including NUL */
+        BYTE(0x00) BYTE(0x33) BYTE(0xDD) BYTE(0x7A) /* Note ID, random for now */
+        BYTE(0x64) BYTE(0x70) BYTE(0x6b) BYTE(0x67) BYTE(0x00) BYTE(0x00) BYTE(0x00) BYTE(0x00) /* Owner: "dpkg" */
+        BYTE(0x73) BYTE(0x79) BYTE(0x73) BYTE(0x74) BYTE(0x65) BYTE(0x6d) BYTE(0x64) BYTE(0x00) /* Value: "systemd" */
+    }
+    .note.package.version ALIGN(8): {
+        BYTE(0x08) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x04) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x00) BYTE(0x33) BYTE(0xDD) BYTE(0x7A)
+        BYTE(0x64) BYTE(0x70) BYTE(0x6b) BYTE(0x67) BYTE(0x00) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x31) BYTE(0x32) BYTE(0x33) BYTE(0x00) /* Value: "123" */
+    }
+    .note.package.distro-name ALIGN(8): {
+        BYTE(0x08) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x08) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x00) BYTE(0x33) BYTE(0xDD) BYTE(0x7A)
+        BYTE(0x64) BYTE(0x70) BYTE(0x6b) BYTE(0x67) BYTE(0x00) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x64) BYTE(0x65) BYTE(0x62) BYTE(0x69) BYTE(0x61) BYTE(0x6e) BYTE(0x00) BYTE(0x00) /* Value: "debian" */
+    }
+    .note.package.distro-version ALIGN(8): {
+        BYTE(0x08) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x04) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x00) BYTE(0x33) BYTE(0xDD) BYTE(0x7A)
+        BYTE(0x64) BYTE(0x70) BYTE(0x6b) BYTE(0x67) BYTE(0x00) BYTE(0x00) BYTE(0x00) BYTE(0x00)
+        BYTE(0x00) BYTE(0x00) BYTE(0x10) BYTE(0x00) /* Value: "10" */
+    }
+}
+INSERT AFTER .note.gnu.build-id;
+"""
+
+import argparse
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('--owner')
+    p.add_argument('--package-name')
+    p.add_argument('--package-version')
+    p.add_argument('--distro-name')
+    p.add_argument('--distro-version')
+
+    return p.parse_args()
+
+def encode_bytes(arr):
+    return ' '.join(f'BYTE(0x{n:02x})' for n in arr)
+
+def encode_bytes_lines(arr, prefix='', label='string'):
+    assert len(arr) % 4 == 0
+    s = bytes(arr).decode()
+    yield prefix + encode_bytes(arr[:4]) + f' /* {label}: {s!r} */'
+    for offset in range(4, len(arr), 4):
+        yield prefix + encode_bytes(arr[offset:offset+4])
+
+def encode_length(s, prefix='', label='string'):
+    n = (len(s) + 1) * 4 // 4
+    n1 = n % 0xFF
+    n2 = n // 0xFF
+    assert n2 < 0xFF
+    return prefix + encode_bytes([n1, n2, 0, 0]) + f' /* Length of {label} including NUL */'
+
+def encode_note_id(arr, prefix=''):
+    assert len(arr) == 4
+    return prefix + encode_bytes(arr) + f' /* Note ID */'
+
+def pad_string(s):
+    return [0] * ((len(s) + 4) // 4 * 4 - len(s))
+
+def encode_string(s, prefix='', label='string'):
+    arr = list(s.encode()) + pad_string(s)
+    yield from encode_bytes_lines(arr, prefix=prefix, label=label)
+
+def encode_note(note_name, note_id, owner, value, prefix=''):
+    l1 = encode_length(owner, prefix=prefix + '    ', label='Owner')
+    l2 = encode_length(value, prefix=prefix + '    ', label='Value')
+    l3 = encode_note_id(note_id, prefix=prefix + '    ')
+    l4 = encode_string(owner, prefix=prefix + '    ', label='Owner')
+    l5 = encode_string(value, prefix=prefix + '    ', label='Value')
+    return [prefix + f'.note.{note_name} ALIGN(8): {{',
+            l1, l2, l3, *l4, *l5,
+            prefix + '}']
+
+NOTE_ID = [0x00, 0x33, 0xDD, 0x7A]
+
+def generate_sections(opts):
+    s1 = encode_note('package',        NOTE_ID, opts.owner,
+                     f'{opts.package_name}\0{opts.package_version}', prefix='    ')
+    s2 = encode_note('package.distro', NOTE_ID, opts.owner,
+                     f'{opts.distro_name}\0{opts.distro_version}', prefix='    ')
+    return ['SECTIONS', '{',
+            *s1,
+            *s2,
+            '}', 'INSERT AFTER .note.gnu.build-id;']
+
+if __name__ == '__main__':
+    opts = parse_args()
+    lines = generate_sections(opts)
+
+    print('\n'.join(lines))
