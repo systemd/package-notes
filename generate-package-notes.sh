25ea3a8a1b466fb8b73c1d7e6a03ba79f9895bac
Luca Boccassi <luca.boccassi@microsoft.com>
2022-01-23 18:43:47 +0000

    shell: support --name arguments
    Add support for well-known parameters to allow for shortcuts, and
    also for arbitrary '--key value' paris.
    
    Keep supporting positional parameters for backward compatibility.

--- generate-package-notes.sh
+++ generate-package-notes.sh
@@ -1,6 +1,97 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC0-1.0
 
+json=
+
+help() {
+    echo "Usage: $0 [OPTION]..."
+    echo "Generate a package notes linker script from specified metadata."
+    echo
+    echo "  -h, --help                      display this help and exit"
+    echo "      --package-type TYPE         set the package type (e.g. 'rpm' or 'deb')"
+    echo "      --package-name NAME         set the package name"
+    echo "      --package-version VERSION   set the package version"
+    echo "      --package-architecture ARCH set the package architecture"
+    echo "      --NAME VALUE                set an arbitrary name/value pair"
+}
+
+invalid_argument() {
+    printf 'ERROR: "%s" requires a non-empty option argument.\n' "${1}" >&2
+    exit 1
+}
+
+append_parameter() {
+    if [ -z "${2}" ]; then
+        invalid_argument "${1}"
+    fi
+
+    # Posix-compatible substring check
+    case "$json" in
+        *"\"${1}\":"*) echo "Duplicated argument: --${1}"; exit 1 ;;
+    esac
+
+    if [ -z "${json}" ]; then
+        json="{\"${1}\":\"${2}\""
+    else
+        json="${json},\"${1}\":\"${2}\""
+    fi
+}
+
+# Support the same fixed parameters as the python script
+parse_options() {
+    while :; do
+        case $1 in
+            -h|-\?|--help)
+                help
+                exit
+                ;;
+            --package-type)
+                append_parameter "type" "${2}"
+                shift
+                ;;
+            --package-name)
+                append_parameter "name" "${2}"
+                shift
+                ;;
+            --package-version)
+                append_parameter "version" "${2}"
+                shift
+                ;;
+            --package-architecture)
+                append_parameter "architecture" "${2}"
+                shift
+                ;;
+            --cpe)
+                append_parameter "osCpe" "${2}"
+                shift
+                ;;
+            --debug-info-url)
+                append_parameter "debugInfoUrl" "${2}"
+                shift
+                ;;
+            --*)
+                # Allow passing arbitrary name/value pairs
+                append_parameter "$(echo "${1}" | cut -c 3-)" "${2}"
+                shift
+                ;;
+            -*)
+                printf 'WARNING: Unknown option (ignored): %s\n' "${1}" >&2
+                ;;
+            *)
+                break
+        esac
+
+        shift
+    done
+
+    # Terminate the JSON object
+    if [ -n "${json}" ]; then
+        json="${json}}"
+    fi
+
+    return "$#"
+}
+
 pad_string() {
     for _ in $(seq "$1"); do
         printf ' BYTE(0x00)'
@@ -46,11 +137,18 @@ write_script() {
     printf 'INSERT AFTER .note.gnu.build-id;\n'
 }
 
-# Not supported on every distro
-if [ ! -r /usr/lib/system-release-cpe ]; then
-    cpe="$(cat /usr/lib/system-release-cpe)"
-    json_cpe=",\"osCpe\": \"${cpe}\""
+if ! parse_options "$@" && [ "$#" -gt 0 ]; then
+    # Not supported on every distro
+    if [ -r /usr/lib/system-release-cpe ]; then
+        cpe="$(cat /usr/lib/system-release-cpe)"
+        json_cpe=",\"osCpe\":\"${cpe}\""
+    fi
+
+    # old-style invocation with positional parameters for backward compatibility
+    json="$(printf '{"type":"rpm","name":"%s","version":"%s","architecture":"%s"%s}' "$1" "$2" "$3" "$json_cpe")"
+elif [ -z "${json}" ]; then
+    help
+    exit 1
 fi
 
-json="$(printf '{"type":"rpm","name":"%s","version":"%s","architecture":"%s"%s}' "$1" "$2" "$3" "$json_cpe")"
 write_script "$json"
