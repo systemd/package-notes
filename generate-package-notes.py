d084887fdd62bba28809347efb8599911fd3fbaa
Luca Boccassi <luca.boccassi@microsoft.com>
2022-01-31 23:45:48 +0000

    version 0.5

--- generate-package-notes.py
+++ generate-package-notes.py
@@ -53,7 +53,7 @@ INSERT AFTER .note.gnu.build-id;
 See https://systemd.io/COREDUMP_PACKAGE_METADATA/ for details.
 """
 
-__version__ = '0.4'
+__version__ = '0.5'
 
 import argparse
 import itertools
