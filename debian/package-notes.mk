2b8d3cdfed76951aa3dc026add0b48e135d15652
Luca Boccassi <bluca@debian.org>
2022-11-28 22:09:33 +0000

    debian: check for binutils version

--- debian/package-notes.mk
+++ debian/package-notes.mk
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: CC0-1.0
 # Include from debian/rules to use with dh_package_notes.
 # See dh_package_notes(1) for details
+
+# binutils 2.39 is required for --package-metadata=
+ifeq (0, $(shell dpkg --compare-versions $$(dpkg-query -f '$${Version}' -W binutils) ge 2.39; echo $$?))
 include /usr/share/dpkg/vendor.mk
 ifeq (, $(filter nocheck, $(DEBUGINFOD_URLS)))
 ifeq ($(DEB_VENDOR),Ubuntu)
@@ -11,6 +14,8 @@ endif
 endif
 export DEB_SOURCE_PACKAGE_VERSION=$(shell dpkg-parsechangelog -S Version)
 export DEB_SOURCE_PACKAGE_NAME=$(shell dpkg-parsechangelog -S Source)
-export DEB_LDFLAGS_MAINT_APPEND+= -specs=/usr/share/debhelper/dh_package_notes/debian-package-notes.specs
+# Set by /usr/share/dpkg/vendor.mk
 export DEB_VENDOR
+export DEB_LDFLAGS_MAINT_APPEND+= -specs=/usr/share/debhelper/dh_package_notes/debian-package-notes.specs
+endif
 
