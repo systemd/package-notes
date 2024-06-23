bec3aa86960b98c8f89fa1aa2197c29371cd7cec
Luca Boccassi <bluca@debian.org>
2024-06-23 17:17:34 +0100

    d/package-notes.mk: change to no-op on Ubuntu
    It was implemented in dpkg-buildpackage itself, so skip the explicit helper
    to avoid duplication

--- debian/package-notes.mk
+++ debian/package-notes.mk
@@ -4,21 +4,20 @@
 
 # llvm does not support spec files
 ifeq ( ,$(filter lld, $(LD)))
+# Ubuntu implemented this in dpkg-buildpackage, make this a no-op to avoid duplication
+include /usr/share/dpkg/vendor.mk
+ifneq ($(DEB_VENDOR),Ubuntu)
 # binutils 2.39 is required for --package-metadata=
 ifeq (0, $(shell dpkg --compare-versions $$(dpkg-query -f '$${Version}' -W binutils) ge 2.39; echo $$?))
-include /usr/share/dpkg/vendor.mk
 ifeq (, $(filter nocheck, $(DEBUGINFOD_URLS)))
-ifeq ($(DEB_VENDOR),Ubuntu)
-export DEBUGINFOD_URLS=https://debuginfod.ubuntu.com
-else
 export DEBUGINFOD_URLS=https://debuginfod.debian.net
-endif
 endif
 export DEB_SOURCE_PACKAGE_VERSION=$(shell dpkg-parsechangelog -S Version)
 export DEB_SOURCE_PACKAGE_NAME=$(shell dpkg-parsechangelog -S Source)
 # Set by /usr/share/dpkg/vendor.mk
 export DEB_VENDOR
 export DEB_LDFLAGS_MAINT_APPEND+= -specs=/usr/share/debhelper/dh_package_notes/debian-package-notes.specs
+endif
 endif
 endif
 
